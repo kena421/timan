3f04eb6970c2bfa6cc84c27dcd0ac6099f45f173
Kedar Nath <kena421@gmail.com>
2026-05-09 17:54:50 +0530

    chore: release v1.0.1

--- Formula/timan.rb
+++ Formula/timan.rb
@@ -1,8 +1,8 @@
 class Timan < Formula
   desc "Minimalist HUD Time Manager for macOS"
   homepage "https://github.com/kena421/timan"
-  url "https://github.com/kena421/timan/archive/refs/tags/v1.0.0.tar.gz"
-  version "1.0.0"
+  url "https://github.com/kena421/timan/archive/refs/tags/v1.0.1.tar.gz"
+  version "1.0.1"
   sha256 "d6fc7f1e303305542d93c9f542306c50f8906570280065963b3b08f011956aee"
   license "MIT"
 
